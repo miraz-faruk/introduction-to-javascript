69d15c2aeb2d62487cbdf25628f7dd1a4d922417
mirazfaruk <mirazfaruk@gmail.com>
2025-04-17 15:06:10 +0600

    Types of variables

---
+++ types.js
@@ -0,0 +1,26 @@
+// String
+var name = 'Miraz Faruk';
+var country = 'Bangladesh';
+var address = 'Nadda';
+
+console.log(name);
+console.log(country);
+console.log(address);
+console.log(typeof name, typeof country, typeof address);
+
+// Number
+var age = 27;
+var money = 1000;
+var weight = 79;
+
+console.log(age);
+console.log(money);
+console.log(weight);
+console.log(typeof age, typeof money, typeof weight);
+
+// Boolean
+var pass = true;
+var eat = false;
+console.log(pass);
+console.log(eat);
+console.log(typeof pass, typeof eat);
